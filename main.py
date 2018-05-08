1b063acdcde81940e1e0c6ba97a4188282114b22
Trevor Thomas <trevorthomas@mac.com>
2018-05-08 09:25:50 -0400

    got list to actually work

--- main.py
+++ main.py
@@ -4,54 +4,60 @@ import psycopg2
 
 DBNAME = "news"
 
-list = []
+sqlList = []
 
-def query1(list):
+def query1(sqlList):
 # Query to the first question, query should return the three article titles with the most amount of views
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("select * from (select title, count(*) as numofviews from articles, log where articles.slug = substring(log.path, articles.slug) group by articles.title limit 3) t order by numofviews desc")
-    if list:
-        list += c.fetchall()
+    if sqlList:
+        sqlList += c.fetchall()
     else:
-        list = c.fetchall()
+        sqlList = c.fetchall()
     db.close()
-    return list
+    return sqlList
 
-def query2(list):
+def query2(sqlList):
 # Query to the second question, query should return the most popular article authors of all time
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("select * from (select authors.name, count(*) as numofviews from articles join log on articles.slug = substring(log.path, articles.slug) join authors on articles.author = authors.id group by authors.name limit 5) t order by t.numofviews desc")
-    if list:
-        list += c.fetchall()
+    if sqlList:
+        sqlList += c.fetchall()
     else:
-        list = c.fetchall()
+        sqlList = c.fetchall()
     db.close()
-    return list
+    return sqlList
 
-def query3(list):
+def query3(sqlList):
 # Query to the third question on which days did more of 1% requests lead to errors
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("select day, errcount, okcount from (select day, count(case when t.status_code ='ok' then 'ok' end) as okcount, count(case when t.status_code = 'err' then 'err' end) as errcount from (select to_char(log.time::date,'DD mon YYYY') as day, case when log.status = '404 NOT FOUND' then 'err' when log.status = '200 OK' then 'ok' else 'baddata' end as status_code from log) t group by day) t2 where errcount > 0.01 * (errcount + okcount)")
-    if list:
-        list += c.fetchall()
+    if sqlList:
+        list3 = c.fetchall()
+        sqlList.extend(list3)
     else:
-        list = c.fetchall()
+        newlist = c.fetchall()
+        sqlList.extend(newlist)
     db.close()
-    return list
 
-def format(result):
-    errcount = result[0][1]
-    okcount = result[0][2]
-    print(errcount)
+def formatList(result):
+    print type(result)
+    print result
+    errcount = float(result[0][1])
+    print errcount
+#    okcount = float(result[0][2])
+#    errrate = float(errcount/(okcount + errcount))
+#    result[1][0] = format(errrate, '.2f')
+#    print(result[0][3])
 
 def main():
 #    result1 = query1(list)
 #    result2 = query2(list)
-    result3 = query3(list)
-    format(result3)
+    query3(sqlList)
+    formatList(sqlList)
 #    print(result1 + result2 + result3)
 
 
