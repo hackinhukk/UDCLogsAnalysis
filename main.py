d451fbd545eef43bf60b9264f80b1bd49991d45f
Trevor Thomas <trevorthomas@mac.com>
2018-05-08 16:44:41 -0400

    PEP 8 Compliant except 4 queries

--- main.py
+++ main.py
@@ -9,37 +9,47 @@ sqlList = []
 col1_header = []
 col2_header = []
 col3_header = []
+SQLtext1 = "select * from (select title, count(*) as numofviews from articles, log where articles.slug = substring(log.path, 10, char_length(log.path)) group by articles.title limit 3) t order by numofviews desc"
+SQLtext2 = "select * from (select authors.name, count(*) as numofviews from articles join log on articles.slug = substring(log.path, 10, char_length(log.path)) join authors on articles.author = authors.id group by authors.name limit 5) t order by t.numofviews desc"
+SQLtext3 = "select day, errcount, okcount from (select day, count(case when t.status_code ='ok' then 'ok' end) as okcount, count(case when t.status_code = 'err' then 'err' end) as errcount from (select to_char(log.time::date,'DD mon YYYY') as day, case when log.status = '404 NOT FOUND' then 'err' when log.status = '200 OK' then 'ok' else 'baddata' end as status_code from log) t group by day) t2 where errcount > 0.01 * (errcount + okcount)"
+
 
 def query1(sqlList):
-# Query to the first question, query should return the three article titles with the most amount of views
-# substr (arg, characters in /articles/, limit character query to length of the string)
-#    string = "Question 1 table with article title, followed by the most amount of views"
+    # Query to the first question, query should return
+    # the three article titles with the most amount of views
+    # substr (arg, characters in /articles/,
+    # limit character query to length of the string)
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select * from (select title, count(*) as numofviews from articles, log where articles.slug = substring(log.path, 10, char_length(log.path)) group by articles.title limit 3) t order by numofviews desc")
+    c.execute(SQLtext1)
     list1 = c.fetchall()
     col_names = [desc[0] for desc in c.description]
     sqlList.append(col_names + list1)
     col1_header.append(col_names)
     db.close()
 
+
 def query2(sqlList):
-# Query to the second question, query should return the most popular article authors of all time
-# substr (arg, characters in /articles/, limit character query to length of the string
+    # Query to the second question, query should return
+    # the most popular article authors of all time
+    # substr (arg, characters in /articles/, limit character
+    # query to length of the string
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select * from (select authors.name, count(*) as numofviews from articles join log on articles.slug = substring(log.path, 10, char_length(log.path)) join authors on articles.author = authors.id group by authors.name limit 5) t order by t.numofviews desc")
+    c.execute(SQLtext2)
     list2 = c.fetchall()
     col_names = [desc[0] for desc in c.description]
     sqlList.append(col_names + list2)
     col2_header.append(col_names)
     db.close()
 
+
 def query3(sqlList):
-# Query to the third question on which days did more of 1% requests lead to errors
+    # Query to the third question on which days did more of
+    # 1% requests lead to errors
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select day, errcount, okcount from (select day, count(case when t.status_code ='ok' then 'ok' end) as okcount, count(case when t.status_code = 'err' then 'err' end) as errcount from (select to_char(log.time::date,'DD mon YYYY') as day, case when log.status = '404 NOT FOUND' then 'err' when log.status = '200 OK' then 'ok' else 'baddata' end as status_code from log) t group by day) t2 where errcount > 0.01 * (errcount + okcount)")
+    c.execute(SQLtext3)
     list3 = c.fetchall()
     col_names = [desc[0] for desc in c.description]
     sqlList.append(col_names + list3)
@@ -47,17 +57,13 @@ def query3(sqlList):
     col3_header.append('error rate in %')
     db.close()
 
-def formatList3(sqlList):
 
-#    print type(sqlList)
-#    print sqlList
+def formatList3(sqlList):
     errcount = Decimal(sqlList[2][3][1])
-#    print errcount
-#    print type(errcount)
     okcount = Decimal(sqlList[2][3][2])
-#    print okcount
     errrate = Decimal(errcount/(okcount + errcount))
     return errrate
+
 
 def queryAll(sqlList):
     query1(sqlList)
@@ -65,6 +71,7 @@ def queryAll(sqlList):
     query3(sqlList)
     rate = formatList3(sqlList)
     return rate
+
 
 def printTableReg(table, col_header):
     for l in table:
@@ -76,11 +83,14 @@ def printTableReg(table, col_header):
                 print col_header[0]
             outerCount += 1
 
+
 def printTable1(table1):
     printTableReg(table1, col1_header)
 
+
 def printTable2(table2):
     printTableReg(table2, col2_header)
+
 
 def printTable3(table3, errrate):
     for l in table3:
@@ -95,6 +105,7 @@ def printTable3(table3, errrate):
                 print col3_header
             outerCount += 1
 
+
 def printOutput(sqlList, errrate):
     table1 = sqlList[0:1]
     table2 = sqlList[1:2]
@@ -105,12 +116,12 @@ def printOutput(sqlList, errrate):
     print "\n"
     printTable3(table3, errrate)
 
+
 def main():
     getcontext().prec = 3
     rate = queryAll(sqlList)
     printOutput(sqlList, rate)
-#    print rate
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
